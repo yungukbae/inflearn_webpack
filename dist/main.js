cd60b6842ba6c5c27d503e5a7dbe88ab23a3a747
yungukbae <yungukbae@gmail.com>
2022-09-18 23:14:32 +0900

    feat: webpack plugin

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,8 @@
+/**
+ * 이것은 BannerPlugin이 처리한 결과입니다.
+ * Build Date: 2019-10-10
+ */
+
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
